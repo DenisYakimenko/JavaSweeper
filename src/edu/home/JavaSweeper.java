98d151f7fcad5e7eacf0d7dba8e0b5f2f0bb771d
DenisYakimenko <dyakimenko@mail.ru>
2022-07-25 00:52:23 +0300

    add new JPanel

--- src/edu/home/JavaSweeper.java
+++ src/edu/home/JavaSweeper.java
@@ -1,20 +1,27 @@
 package edu.home;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+
+    JPanel panel;
     public static void main(String[] args) {
-        new JavaSweeper().setVisible(true);
+        new JavaSweeper();
 
     }
 
     //конструктор закрытия программы
     public JavaSweeper (){
+
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500,300));
+        add(panel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);//запрет на изменение размера формы
-        setSize(320,345);
+        pack();//заставляет сработать panel.setPreferredSize (new Dimension(500,300))
+        setLocationRelativeTo(null);
     }
 }
