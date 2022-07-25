40e1e83d4a9b5b1b8c60411a69d6889f2575425c
DenisYakimenko <dyakimenko@mail.ru>
2022-07-25 20:53:33 +0300

    add COLS, ROWS, IMAGE_SIZE

--- src/edu/home/JavaSweeper.java
+++ src/edu/home/JavaSweeper.java
@@ -5,7 +5,10 @@ import java.awt.*;
 
 public class JavaSweeper extends JFrame {
 
-   private JPanel panel;
+    private final int COLS =15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;//обозначили константой размер картинки
+    private JPanel panel;
     public static void main(String[] args) {
         new JavaSweeper();
 
@@ -26,10 +29,13 @@ public class JavaSweeper extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(getImage("bomb"),0,0,this);
-                g.drawImage(getImage("num1"),50,0,this);
+                g.drawImage(getImage("num1"),IMAGE_SIZE,0,this);
+
             }
         };
-        panel.setPreferredSize(new Dimension(500,300));
+        panel.setPreferredSize(new Dimension(
+                COLS*IMAGE_SIZE,
+                ROWS*IMAGE_SIZE));
         add(panel);
     }
 
