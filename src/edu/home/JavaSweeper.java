df943990629a1b802d0daf5b8e1419528baae81a
DenisYakimenko <dyakimenko@mail.ru>
2022-07-25 17:09:33 +0300

    refactoring getImage

--- src/edu/home/JavaSweeper.java
+++ src/edu/home/JavaSweeper.java
@@ -25,7 +25,8 @@ public class JavaSweeper extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(getImage(),0,0,this);
+                g.drawImage(getImage("bomb"),0,0,this);
+                g.drawImage(getImage("num1"),50,0,this);
             }
         };
         panel.setPreferredSize(new Dimension(500,300));
@@ -42,8 +43,8 @@ public class JavaSweeper extends JFrame {
     }
 
     //метод ппоказывающий картинку
-    private Image getImage(){
-     ImageIcon icon = new ImageIcon("res/img/bomb.png");
+    private Image getImage(String name){
+     ImageIcon icon = new ImageIcon("res/img/"+name+".png");
      return icon.getImage();
     }
 }
