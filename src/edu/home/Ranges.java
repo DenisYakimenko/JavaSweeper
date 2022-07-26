79194449251c928a61f752978e8f6c724d6e9db2
DenisYakimenko <dyakimenko@mail.ru>
2022-07-27 02:16:58 +0300

    add class Ranges

---
+++ src/edu/home/Ranges.java
@@ -0,0 +1,14 @@
+package edu.home;
+
+public class Ranges
+{
+    private Coord size;
+    public void setSize(Coord size){
+        this.size = size;
+    }
+
+    public Coord getSize(){
+        return  size;
+
+    }
+}
